f81b8a03b037dd61c788ee6ddb13587b06396834
Asif <asif.sk@arkatiss.com>
2026-06-26 21:09:54 +0530

    Refractor: Refactoring form111 retrieve By ID - Adding def_id to the response

--- csa_store/app/api/form111/form111_retrieve_by_id.py
+++ csa_store/app/api/form111/form111_retrieve_by_id.py
@@ -99,6 +99,7 @@ def form111_retrieve_by_id(
                     "return_value": 0,
                     "error_message": "",
                     "data": {
+                        "def_id": request.def_id,
                         "def_store": row[0],
                         "def_date": row[1],
                         "def_form_type": row[2],
